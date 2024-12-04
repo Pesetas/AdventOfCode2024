c2d53e22fd8fda237f78a838293894dc9ffc940f
Antonio Ramirez Martin <anramir2@student.42malaga.com>
2024-12-04 15:50:56 +0100

    day4-2

---
+++ day4-2/day4-2.c
@@ -0,0 +1,65 @@
+/*--------------------#
+# Advent of code 2024 #
+# Dia 4, ejercicio 2  #
+#---------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAX_LINE_LENGTH 150
+
+static void	find(char matriz[MAX_LINE_LENGTH][MAX_LINE_LENGTH], int *total)
+{
+	for(int x = 0; x < MAX_LINE_LENGTH; x++)
+	{
+		for(int y = 0; y < MAX_LINE_LENGTH; y++)
+		{
+			if (matriz[x][y] == 'A')
+			{
+				if ((matriz[x-1][y-1] == 'M') && (matriz[x-1][y+1] == 'M') 
+				&& (matriz[x+1][y-1] == 'S') && (matriz[x+1][y+1] == 'S')) //up
+					*total += 1;
+				if ((matriz[x+1][y-1] == 'M') && (matriz[x+1][y+1] == 'M') 
+				&& (matriz[x-1][y-1] == 'S') && (matriz[x-1][y+1] == 'S')) //down
+					*total += 1;
+				if ((matriz[x-1][y-1] == 'M') && (matriz[x+1][y-1] == 'M') 
+				&& (matriz[x-1][y+1] == 'S') && (matriz[x+1][y+1] == 'S')) //right
+					*total += 1;
+				if ((matriz[x-1][y+1] == 'M') && (matriz[x+1][y+1] == 'M') 
+				&& (matriz[x-1][y-1] == 'S') && (matriz[x+1][y-1] == 'S')) //left
+					*total += 1;
+			}
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	FILE	*file;
+	char	buffer[MAX_LINE_LENGTH][MAX_LINE_LENGTH];
+    int     i;
+    int		total;
+
+	(void)argc;
+	if (!argv[1])
+	{
+		fprintf(stderr, "You must input a file as an argument.");
+		return (1);
+	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error opening text file");
+		exit(1);
+	}
+    i = 0;
+    while (fgets(buffer[i], MAX_LINE_LENGTH, file) != 0)
+        i++;
+    fclose(file);
+	total = 0;
+	i = 0;
+	find(buffer, &total);
+	printf("%d", total);
+    return (0);
+}
